62ca3a1f39a447c58d3879cb68878a6395f95ad1
Eli <yuchien.elliot@gmail.com>
2022-04-01 15:38:34 -0700

    updated

--- password_ans.py
+++ password_ans.py
@@ -1,6 +1,6 @@
 password = "a123456"
 i = 3 # times left
-while True:
+while True:   # can use While True or While i > 0
 	pwd = input('Please enter your password: ')	
 	if pwd == password:
 		print('Successfully Signed In!')
@@ -9,6 +9,6 @@ while True:
 		i = i - 1
 		print('Wrong! You have ', i,' times left!')
 		if i == 0:
-			break
+			break    # if we use While i > 0, then the last condition can be removed
 
 
